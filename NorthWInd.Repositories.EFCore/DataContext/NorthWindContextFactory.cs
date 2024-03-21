8bf599ab8d99678450d024b377036ef1d1296cb6
JuanEstebanGonzalez19 <juanestebangonzalezsierra0327@gmail.com>
2024-03-21 11:44:00 -0500

    DataContext / Create NothWindContex Factoryt

---
+++ NorthWInd.Repositories.EFCore/DataContext/NorthWindContextFactory.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NorthWind.Repositories.EFCore.DataContext
+{
+    class NorthWindContextFactory : 
+        IDesignTimeDbContextFactory<NorthWindContext>
+    {
+        public NorthWindContext CreateDbContext(string[] args)
+        {
+            //throw new NotImplementedException();
+            var OptionsBuilder =
+                new DbContextOptionsBuilder<NorthWindContext>();
+            OptionsBuilder.UseSqlServer(
+                "Serve = (localdb) \\ mssqllocaldb;Database=NorthWindDB");
+            return new NorthWindContext(OptionsBuilder.Options);
+        }
+    } 
+}
